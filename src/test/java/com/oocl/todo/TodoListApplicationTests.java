f55152956d5dbf7b26bbc74387371d0ae3aac931
roy <1216172441@qq.com>
2020-08-07 01:00:12 +0800

    [todo-api][roy-lebron] feature: create a springboot project

---
+++ src/test/java/com/oocl/todo/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
